5b1eeda9ea6226d0843e9653f6389b74527d5bb7
Sjir Bagmeijer <sjir.bagmeijer@ulyaoth.com>
2024-08-08 20:33:12 +0200

    update to help

--- nyuryoku/commands/user/help.py
+++ nyuryoku/commands/user/help.py
@@ -5,6 +5,7 @@ def get_user_help_menu():
         "Thank you for using Mamoru your security assistant and more!\n\n"
         "Here are the user specific commands you can use:\n"
         "User Commands:\n"
-        "- `user help`: Display this help menu\n\n"
+        "- `user help`: Display this help menu\n"
+        "- `user myinfo`: Display your info and access level\n\n"       
         "If you have any questions, please reach out to us."
     )
