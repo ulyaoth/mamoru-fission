451502f0a0e52f634f71720a8918db2646453797
Sjir Bagmeijer <sjir.bagmeijer@ulyaoth.com>
2024-08-11 09:48:59 +0200

    lazy import to avoid errors

--- nyuryoku/auth_handlers/auth_methods.py
+++ nyuryoku/auth_handlers/auth_methods.py
@@ -1,8 +1,5 @@
 # auth_handlers/auth_methods.py
 
-from slack_specific.slack_user_verification import verify_slack_user
-from auth_handlers.microsoft_entra import verify_microsoft_entra_user
-
 # Function to read the ConfigMap values
 def read_config(key):
     config_path = f"/configs/default/mamoru-configmap/{key}"
@@ -17,8 +14,10 @@ auth_method = read_config('AUTH_METHOD')
 
 def verify_user(user_id, source):
     if auth_method == 'slack_user_verification':
+        from slack_specific.slack_user_verification import verify_slack_user
         return verify_slack_user(user_id)
     elif auth_method == 'microsoft_entra':
+        from auth_handlers.microsoft_entra import verify_microsoft_entra_user
         return verify_microsoft_entra_user(user_id, source)
     else:
         raise ValueError("Invalid authentication method")
